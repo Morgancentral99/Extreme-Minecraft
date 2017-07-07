75a46591dc74c544da21f49a52e4ed6188790f51
Morgan <morgancentral99@gmail.com>
2017-07-07 10:49:39 -0600

    Revert "Added PlayerJoinLeaveEvent & GitIgnore"
    This reverts commit e352dc82feaa7ce71e405914d70adea32c21514e.

--- CreativeCore/src/me/morgancentral99/creative/listeners/OnPlayerJoinLeaveEvent.java
+++ CreativeCore/src/me/morgancentral99/creative/listeners/OnPlayerJoinLeaveEvent.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.morgancentral99.creative.CreativeCore;
 import me.morgancentral99.creative.PlayerOverride;
@@ -22,15 +21,7 @@ public class OnPlayerJoinLeaveEvent implements Listener {
 	}
 	if(!p.hasPlayedBefore()) {
 		Bukkit.broadcastMessage(CreativeCore.format(CreativeCore.getPrefix() + "Wisp Something can go here"));
-	} 
-	//Player Join message
-	Bukkit.broadcastMessage(CreativeCore.format(""));
 	}
-	
-	@EventHandler
-	public void onPlayerLeaveEvent(PlayerQuitEvent e) {
-		//Player Leave message
-		Bukkit.broadcastMessage(CreativeCore.format(""));
 	}
 
 }
