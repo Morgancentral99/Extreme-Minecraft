e352dc82feaa7ce71e405914d70adea32c21514e
Morgan <morgancentral99@gmail.com>
2017-07-07 10:48:44 -0600

    Added PlayerJoinLeaveEvent & GitIgnore

--- CreativeCore/src/me/morgancentral99/creative/listeners/OnPlayerJoinLeaveEvent.java
+++ CreativeCore/src/me/morgancentral99/creative/listeners/OnPlayerJoinLeaveEvent.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.morgancentral99.creative.CreativeCore;
 import me.morgancentral99.creative.PlayerOverride;
@@ -21,7 +22,15 @@ public class OnPlayerJoinLeaveEvent implements Listener {
 	}
 	if(!p.hasPlayedBefore()) {
 		Bukkit.broadcastMessage(CreativeCore.format(CreativeCore.getPrefix() + "Wisp Something can go here"));
+	} 
+	//Player Join message
+	Bukkit.broadcastMessage(CreativeCore.format(""));
 	}
+	
+	@EventHandler
+	public void onPlayerLeaveEvent(PlayerQuitEvent e) {
+		//Player Leave message
+		Bukkit.broadcastMessage(CreativeCore.format(""));
 	}
 
 }
